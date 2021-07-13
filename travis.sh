5c83b6da292e1170c627b4b1e4c50996648ac06b
Alex Schittko <aschittko@thrivepos.com>
2021-07-13 02:07:05 -0500

    travis updates

--- travis.sh
+++ travis.sh
@@ -5,4 +5,6 @@ set -o pipefail
 
 bash build.sh
 bash test.sh
-docker push alex4108/library-db
+if [[ "${TRAVIS_PULL_REQUEST}" == "false" && "${TRAVIS_BRANCH}" == "master" ]]; then
+    docker push alex4108/library-frontend
+fi
